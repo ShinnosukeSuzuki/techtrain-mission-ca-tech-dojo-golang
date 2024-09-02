d1c952fabe521ce249b0bc5cc2b7df7ec6061340
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-09-02 20:48:53 +0900

    DB設計の修正
    - 各tableのidをcarcharでユニークに変更

--- db/init.sql
+++ db/init.sql
@@ -1,22 +1,22 @@
 # ユーザ情報を格納するためのテーブル
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     token VARCHAR(255) NOT NULL UNIQUE
 );
 
 # キャラクター情報を格納するためのテーブル
 CREATE TABLE IF NOT EXISTS characters (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     probability FLOAT NOT NULL
 );
 
 # ユーザがキャラクターを引いた記録を格納するためのテーブル
-CREATE TABLE IF NOT EXISTS results (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    character_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS users_characters (
+    id VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    character_id VARCHAR(255) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (character_id) REFERENCES characters(id)
 );
