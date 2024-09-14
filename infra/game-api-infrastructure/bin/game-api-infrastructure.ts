faad44a821e30872ac6b5bcbb6caab94e265f4ea
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-09-14 20:32:13 +0900

    Game API関係スタックの作成

--- infra/game-api-infrastructure/bin/game-api-infrastructure.ts
+++ infra/game-api-infrastructure/bin/game-api-infrastructure.ts
@@ -5,17 +5,11 @@ import { GameApiInfrastructureStack } from '../lib/game-api-infrastructure-stack
 
 const app = new cdk.App();
 new GameApiInfrastructureStack(app, 'GameApiInfrastructureStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  environment: process.env.ENV || 'Dev' as string,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
+
+cdk.Tags.of(app).add("ENV", process.env.ENV || "Dev");
