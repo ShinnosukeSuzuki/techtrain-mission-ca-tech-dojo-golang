331f6c6e34a46bad252175757c6ed0f3eb611eec
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-09-14 09:40:16 +0900

    docker関係
    - go api serverのDockerfile作成
    - go api serverコンテナを作成するようにdocker-compose.yamlに追加
    - db接続関数でローカル実行とコンテナ実行で環境変数の読み取り先を変更する

--- db/db.go
+++ db/db.go
@@ -3,24 +3,27 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func NewDB() (*sql.DB, error) {
-	// .envファイルから環境変数を取得
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
+	// DOCKER_ENV 環境変数が設定されていない場合は .env ファイルを読み込む(ローカル環境では .env ファイルを読み込むが、Docker環境ではコンテナに設定された環境変数を読み込む)
+	if os.Getenv("DOCKER_ENV") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			return nil, fmt.Errorf("error loading .env file: %v", err)
+		}
 	}
 
 	var (
 		dbUser     = os.Getenv("USERNAME")
 		dbPassword = os.Getenv("USERPASS")
 		dbDatabase = os.Getenv("DATABASE")
-		dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+		dbHost     = os.Getenv("DBHOST")
+		dbPort     = os.Getenv("DBPORT")
+		dbConn     = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbDatabase)
 	)
 
 	db, err := sql.Open("mysql", dbConn)
