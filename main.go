61febfe3b269984972651a6c3a3e930a588453b5
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-10-01 23:13:29 +0900

    キャッシュ導入におけるmain.goの修正

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/ShinnosukeSuzuki/techtrain-mission-ca-tech-dojo-golang/api"
+	"github.com/ShinnosukeSuzuki/techtrain-mission-ca-tech-dojo-golang/cache"
 	"github.com/ShinnosukeSuzuki/techtrain-mission-ca-tech-dojo-golang/db"
 	_ "github.com/go-sql-driver/mysql"
 	"golang.org/x/sync/errgroup"
@@ -28,8 +29,24 @@ func main() {
 		log.Fatalf("server shutdown because db connection failed: %v", err)
 	}
 
+	// キャッシュを初期化
+	characterCache, err := cache.NewCharacterProbabilityCache(
+		os.Getenv("REGION"),
+		os.Getenv("BUCKET_NAME"),
+		os.Getenv("FILE_PATH"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to initialize cache: %v", err)
+	}
+
+	// キャッシュの定期更新を開始
+	err = characterCache.StartCron()
+	if err != nil {
+		log.Fatalf("Failed to start cron cache update: %v", err)
+	}
+
 	// ルーターを作成
-	e := api.NewRouter(db)
+	e := api.NewRouter(db, characterCache)
 
 	// シグナルを受け取るためのコンテキストを作成
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, os.Interrupt, os.Kill)
@@ -43,6 +60,13 @@ func main() {
 		<-ctx.Done()
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 		defer cancel()
+
+		// キャッシュの定期更新を停止
+		if err := characterCache.StopCron(); err != nil {
+			log.Printf("Failed to stop cron cache update: %v", err)
+		}
+
+		// サーバーをシャットダウン
 		return e.Shutdown(shutdownCtx)
 	})
 
