17d93aa5df7e496574b576b5e74c21fde52dd2ea
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-11-01 11:34:20 +0900

    DB schemaの変更
    各テーブルのidとusersテーブルのtokenをBINARYに変更

--- db/sample-data.sql
+++ db/sample-data.sql
@@ -1,22 +1,22 @@
 # ユーザ情報を格納するためのテーブル
 CREATE TABLE IF NOT EXISTS users (
-    id VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
+    id BINARY(16) NOT NULL UNIQUE PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL UNIQUE
+    token BINARY(16) NOT NULL UNIQUE
 );
 
 # キャラクター情報を格納するためのテーブル
 CREATE TABLE IF NOT EXISTS characters (
-    id VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
+    id BINARY(16) NOT NULL UNIQUE PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     probability FLOAT NOT NULL
 );
 
 # ユーザがキャラクターを引いた記録を格納するためのテーブル
 CREATE TABLE IF NOT EXISTS users_characters (
-    id VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL,
-    character_id VARCHAR(255) NOT NULL,
+    id BINARY(16) NOT NULL UNIQUE PRIMARY KEY,
+    user_id BINARY(16) NOT NULL,
+    character_id BINARY(16) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (character_id) REFERENCES characters(id)
 );
